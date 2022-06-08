38419bc5bd0e612ce1ea985a2b6c1f35e4e43b6b
ireck22 <dragon9029@gmail.com>
2022-06-08 16:36:10 +0800

    第四題寫好

--- q4.php
+++ q4.php
@@ -1,3 +1,32 @@
 <?php
-    $result=[77,5,5,22,13,55,97,4,796,10,1,0,9];
-    
+
+function slouction($n_array)
+{
+    $len = count($n_array);
+    if ($len <= 1) {
+        return $n_array;
+    }
+
+    $key = $n_array[0];
+    $right = [];
+    $left = [];
+
+    for ($i = 1; $i < $len; $i++) {
+        if($key<$n_array[$i]){
+            $right[]=$n_array[$i];
+        }else{
+            $left[]=$n_array[$i];
+        }
+    }
+
+    $right = slouction($right);
+    $left = slouction($left);
+
+    $finish = array_merge($left,array($key), $right);
+    return $finish;
+}
+
+$result = [77, 5, 5, 22, 13, 55, 97, 4, 796, 10, 1, 0, 9];
+$finish = slouction($result);
+
+print_r($finish);
